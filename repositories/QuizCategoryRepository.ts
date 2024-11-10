7a6fb840da402f37989434dd6be9703869f1da69
Toni <anton_krizmanic@hotmail.com>
2024-11-10 10:11:47 +0100

    Camel case is important

--- repositories/QuizCategoryRepository.ts
+++ repositories/QuizCategoryRepository.ts
@@ -3,7 +3,7 @@ import { mapDtosToQuizCategories } from '@/mappers/QuizCategoryMapper';
 import prisma from './db';
 
 export const getQuizCategoriesWithQuestions = async (cityAssociationId: number, quizTheme: number) => {
-    const data = await prisma.QuizCategories.findMany({
+    const data = await prisma.quizCategories.findMany({
         where: {
             CityAssociationId: cityAssociationId,
             QuizTheme: quizTheme,
